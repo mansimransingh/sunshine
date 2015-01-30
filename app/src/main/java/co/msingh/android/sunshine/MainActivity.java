ab926e42cce50e91d777600fc2361fc5691d74d9
Mansimran Singh <me@mansimransingh.com>
2015-01-30 01:12:36 +0000

    working weather data in list view:: lesson 2 done

--- app/src/main/java/co/msingh/android/sunshine/MainActivity.java
+++ app/src/main/java/co/msingh/android/sunshine/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,6 +14,12 @@ import android.os.Build;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 
@@ -24,7 +31,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -55,36 +62,5 @@ public class MainActivity extends ActionBarActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
-        private ArrayAdapter<String> mAdapter;
 
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            ArrayList weather = new ArrayList<String>();
-            weather.add("Today - Sunny - 88/63");
-            weather.add("Tomorrow - Sunny - 88/63");
-            weather.add("Weds - Sunny - 88/63");
-            weather.add("Thurs - Sunny - 88/63");
-            weather.add("Fri - Sunny - 88/63");
-            weather.add("Sat - Sunny - 88/63");
-            weather.add("Sun - Sunny - 88/63");
-            weather.add("Mon - Sunny - 88/63");
-
-
-             mAdapter = new ArrayAdapter<String>(getActivity(),
-                    R.layout.list_item_forecast, R.id.list_item_forecast_textview, weather);
-
-            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
-
-            listView.setAdapter(mAdapter);
-
-            return rootView;
-        }
-    }
 }
