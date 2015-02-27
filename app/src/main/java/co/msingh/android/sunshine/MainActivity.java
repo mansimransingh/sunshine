1b48a8bdd4aaf94e721ba97d69c12a153c8cb917
Mansimran Singh <me@mansimransingh.com>
2015-02-27 00:57:20 +0000

    implemented redlines

--- app/src/main/java/co/msingh/android/sunshine/MainActivity.java
+++ app/src/main/java/co/msingh/android/sunshine/MainActivity.java
@@ -46,7 +46,7 @@ public class MainActivity extends ActionBarActivity implements ForecastFragment.
             }
         } else {
             mTwoPane = false;
-
+            getSupportActionBar().setElevation(0f);
         }
 
         ForecastFragment forecastFragment = ((ForecastFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_forecast));
